fa368fc4a26e75fd562cba28f3d56ca7301e4433
ravindragsingh <arien.641@gmail.com>
2018-10-23 22:19:02 -0400

    all cards on click open show

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
  * Create a list that holds all of your cards
  */
 
-var cardList = ["fa fa-bolt", "fa fa-anchor", "fa fa-bicycle", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-cube", "fa fa-leaf", "fa fa-bomb"]
 
 /*
  * Display the cards on the page
@@ -12,43 +11,37 @@ var cardList = ["fa fa-bolt", "fa fa-anchor", "fa fa-bicycle", "fa fa-diamond", 
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(cardList) {
-    var currentIndex = cardList.length,
+function shuffle(array) {
+    var currentIndex = array.length,
         temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = cardList[currentIndex];
-        cardList[currentIndex] = cardList[randomIndex];
-        cardList[randomIndex] = temporaryValue;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
-    return cardList;
+    return array;
 }
 
-// for (i = 0; i < 16; i++) {
-//     document.getElementById("list1").className = cardList[i]
-// }
+allCards = document.querySelectorAll('.card');
+allCards.forEach(function(card) {
+    card.addEventListener('click', function(e) {
+        card.classList.add('open', 'show');
+    })
 
-function refreshPage() {
-    shuffle(cardList);
-}
-counter = 0;
+});
 
-function noOfClicks() {
-    counter = counter + 1
 
-    document.getElementById("moveCount").innerHTML = counter
-}
-
-// /*
-//  * set up the event listener for a card. If a card is clicked:
-//  *  - display the card's symbol (put this functionality in another function that you call from this one)
-//  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-//  *  - if the list already has another card, check to see if the two cards match
-//  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-//  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-//  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-//  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-//  *
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
